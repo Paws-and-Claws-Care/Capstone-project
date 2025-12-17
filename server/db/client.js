082a9b0f449b409cdd3dab9b16067f1838be5901
Alyssa <alyssaradlauer@gmail.com>
2025-12-17 10:37:37 -0700

    started a seed function in seed.js

--- server/db/client.js
+++ server/db/client.js
@@ -1,4 +1,6 @@
-const pg = require('pg');
-const client = new pg.Client(process.env.DATABASE_URL || 'postgres://localhost/your_db_name_here');
+import pg from "pg";
+const client = new pg.Client(
+  process.env.DATABASE_URL || "postgres://localhost/your_db_name_here"
+);
 
-module.exports = client
+module.exports = client;
---
+++ server/db/seed.js
@@ -0,0 +1,9 @@
+import db from "./client.js";
+
+await db.connect();
+await seed();
+await db.end();
+console.log("🌱 Database seeded.");
+async function seed() {
+  //TODO
+}
