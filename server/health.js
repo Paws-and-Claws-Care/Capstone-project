ed3cefb253be70a5f6914ec40dad953654429cba
Nae-Coleman <teranaecoleman@gmail.com>
2026-01-05 12:54:04 -0500

    File created for Health and Wellness

---
+++ server/health.js
@@ -0,0 +1,11 @@
+//   Dog Vitamins and Supplements
+
+createProduct({
+  name: "Pro Plan Dog Food - Digestive Health",
+  description: "High protein chicken and rice",
+  price: 54,
+  category: "food",
+  pet_type: "dog",
+  image_url: "image",
+  quantity: 1,
+});
