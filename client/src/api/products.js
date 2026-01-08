ce2d72b4b3437833ab162df80c8d94473ba95065
Alyssa <alyssaradlauer@gmail.com>
2026-01-08 11:09:47 -0700

    added buttons to test sorting of categories and pet type

--- client/src/api/products.js
+++ client/src/api/products.js
@@ -7,3 +7,16 @@ export async function fetchAllProducts() {
 
   return res.json();
 }
+export async function fetchProductsByCategory(category) {
+  const res = await fetch(`/api/products?category=${category}`);
+  if (!res.ok) throw new Error("Failed to fetch by category");
+  return res.json();
+}
+
+export async function fetchProductsByPetType(petType) {
+  const res = await fetch(
+    `/api/products?pet_type=${encodeURIComponent(petType)}`
+  );
+  if (!res.ok) throw new Error("Failed to fetch products by pet type");
+  return res.json();
+}
--- client/src/pages/Products.jsx
+++ client/src/pages/Products.jsx
@@ -1,5 +1,9 @@
 import { useEffect, useState } from "react";
-import { fetchAllProducts } from "../api/products";
+import {
+  fetchAllProducts,
+  fetchProductsByCategory,
+  fetchProductsByPetType,
+} from "../api/products";
 
 function Products() {
   const [products, setProducts] = useState([]);
@@ -23,9 +27,63 @@ function Products() {
     <div>
       <h2>Products</h2>
 
+      <h2>Products</h2>
+
+      {/* CATEGORY FILTERS */}
+      <button
+        onClick={async () => {
+          const data = await fetchProductsByCategory("treats");
+          console.log("TREATS:", data);
+          setProducts(data);
+        }}
+      >
+        Treats
+      </button>
+
+      <button
+        onClick={async () => {
+          const data = await fetchProductsByCategory("food");
+          setProducts(data);
+        }}
+      >
+        Food
+      </button>
+
+      {/* PET TYPE FILTERS */}
+      <button
+        onClick={async () => {
+          const data = await fetchProductsByPetType("dog");
+          console.log("DOG PRODUCTS:", data);
+          setProducts(data);
+        }}
+      >
+        Dog
+      </button>
+
+      <button
+        onClick={async () => {
+          const data = await fetchProductsByPetType("cat");
+          setProducts(data);
+        }}
+      >
+        Cat
+      </button>
+
+      {/* RESET */}
+      <button
+        onClick={async () => {
+          const data = await fetchAllProducts();
+          setProducts(data);
+        }}
+      >
+        All Products
+      </button>
+
       {products.map((product) => (
         <div key={product.id} style={{ marginBottom: "24px" }}>
           <h3>{product.name}</h3>
+          <p>{product.category}</p>
+          <h3>{product.pet_type}</h3>
           <h4>{product.description}</h4>
 
           <img
@@ -42,3 +100,10 @@ function Products() {
 }
 
 export default Products;
+
+//fetch all products
+
+// category sort by treat
+// const data = await fetchProductsByCategory("treats");
+//         console.log("CATEGORY PRODUCTS:", data);
+//         setProducts(data);
