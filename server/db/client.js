9d07f0b5ecb2c178fb47146d6b7da057d0e2a691
Alyssa <alyssaradlauer@gmail.com>
2026-01-19 10:37:47 -0700

    client.js for deployment

--- server/db/client.js
+++ server/db/client.js
@@ -2,6 +2,10 @@ import { Client } from "pg";
 
 const client = new Client({
   connectionString: process.env.DATABASE_URL,
+  ssl:
+    process.env.NODE_ENV === "production"
+      ? { rejectUnauthorized: false }
+      : false,
 });
 
 export default client;
