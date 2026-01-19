2f097d36d54fc8c96e1bc0e38fb38f649b4b8e94
Alyssa <alyssaradlauer@gmail.com>
2026-01-19 10:48:30 -0700

    updated client.js again

--- server/db/client.js
+++ server/db/client.js
@@ -1,11 +1,10 @@
 import { Client } from "pg";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const client = new Client({
   connectionString: process.env.DATABASE_URL,
-  ssl:
-    process.env.NODE_ENV === "production"
-      ? { rejectUnauthorized: false }
-      : false,
+  ssl: isProd ? { rejectUnauthorized: false } : false,
 });
 
 export default client;
